93304b89198a9fb50ce316fecbe51bc32769b1ff
Aske <burzum17@naver.com>
2021-12-30 20:50:03 +0900

    Initial Commit

---
+++ ASPreviewSlider/ASPreviewSlider.h
@@ -0,0 +1,18 @@
+//
+//  ASPreviewSlider.h
+//  ASPreviewSlider
+//
+//  Created by GeunHwa Lee on 2021/12/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ASPreviewSlider.
+FOUNDATION_EXPORT double ASPreviewSliderVersionNumber;
+
+//! Project version string for ASPreviewSlider.
+FOUNDATION_EXPORT const unsigned char ASPreviewSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASPreviewSlider/PublicHeader.h>
+
+
